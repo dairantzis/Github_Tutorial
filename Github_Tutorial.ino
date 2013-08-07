a729f9842aff542aa647a36af0752f2faea01bf7
Dimitrios Airantzis <dimitrios.airantzis.12@ucl.ac.uk>
2013-08-07 12:00:12 +0100

    Experimented with the ino file.
    Training session. These are the changes I made.
    Personal message:
    I deleted the previous personal message and added my own.
    void loop():
    Changed myValue var from byte type to int type.

--- Github_Tutorial.ino
+++ Github_Tutorial.ino
@@ -1,11 +1,6 @@
 /*
- 12-29-2012
- Spark Fun Electronics
- Nathan Seidle
- 
- This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
- 
- This is bad code but is used to demonstrate how to use repositories on github.
+ 7th August 2013
+ Ammended the file in order to experiment with the way that repositories work on github.
  
  If we have an analog sensor such as a trimpot or flex sensor on A0, this code would attempt
  to read it and output it to the terminal at 9600bps.
@@ -14,6 +9,7 @@
 void setup()
 {
   Serial.begin(9600);
+  Serial.println("Hello world example and ");
   Serial.println("Trimpot read example");
 
   pinMode(A0, INPUT);
@@ -21,7 +17,7 @@ void setup()
 
 void loop() 
 {
-  byte myValue = 0;
+  int myValue = 0;
   myValue = analogRead(A0);
   
   Serial.print("The value is: ");
